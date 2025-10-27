a8c8940ab606a41e55a64fc00d25f5f134833f6a
ET37599 <et37599@jostens.com>
2025-10-27 22:53:21 +0200

    Initialized tg bot

--- src/telegram/bot.ts
+++ src/telegram/bot.ts
@@ -1,41 +1,28 @@
-import { Telegraf } from 'telegraf';
+import { session, Telegraf } from 'telegraf';
 import { message } from 'telegraf/filters';
-
-export enum MenuItemEnum {
-  SAY_HELLO = 'say_hello',
-}
-
-export const BOT_MENU: IMenuItem[][] = [
-  [
-    { text: 'Generate Wallets', callback_data: MenuItemEnum.SAY_HELLO },
-  ]
-]
-
-export interface IMenuItem {
-  text: string;
-  callback_data: MenuItemEnum;
-}
+import console from 'console';
 
 export class TelegramBot {
   private bot: Telegraf;
 
-  get mainMenu(): { parse_mode: string, reply_markup: { inline_keyboard: IMenuItem[][] } } {
-    return { parse_mode: 'MarkdownV2', reply_markup: { inline_keyboard: BOT_MENU } };
-  }
-
   constructor() {
     this.bot = new Telegraf(process.env.TG_BOT_TOKEN || '');
-    this.initializeBot();
+
+    this.init();
   }
 
-  private initializeBot(): void {
-    this.bot.start((ctx) =>
-      ctx.reply('Welcome')
-    );
+  private async init() {
+    this.bot.use(session());
+    this.bot.start((ctx) => ctx.reply('Welcome'));
+    this.initMessageListener();
+    await this.bot.launch();
+  }
 
-    this.bot.help((ctx) => ctx.reply('Send me a sticker'))
-    this.bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-    this.bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-    this.bot.launch()
+  initMessageListener(): void {
+    this.bot.on(message('text'), async (ctx) => {
+      const userText = ctx.message.text;
+      console.log('Received text:', userText);
+      await ctx.reply(`You said: ${userText}`);
+    });
   }
 }
